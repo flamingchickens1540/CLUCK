5f220efb594ba5d97d348e17020c3dd4bf599241
Zach R <92497727+rutmanz@users.noreply.github.com>
2022-09-11 20:13:39 -0700

    make grid member boxes more square

--- www/grid/script/script.js
+++ www/grid/script/script.js
@@ -2,11 +2,11 @@
 function openFullscreen() {
     let elem = document.documentElement
     if (elem.requestFullscreen) {
-      elem.requestFullscreen();
+        elem.requestFullscreen();
     } else if (elem.webkitRequestFullscreen) { /* Safari */
-      elem.webkitRequestFullscreen();
+        elem.webkitRequestFullscreen();
     } else if (elem.msRequestFullscreen) { /* IE11 */
-      elem.msRequestFullscreen();
+        elem.msRequestFullscreen();
     }
 }
 
@@ -16,7 +16,7 @@ function openFullscreen() {
 /* globals clock ping cluckedIn checkAuth */
 let buttonJustPressed = false;
 
-
+let members
 
 async function run() {
     const authed = await checkAuth();
@@ -24,16 +24,12 @@ async function run() {
         document.location.replace("/grid/login");
     }
     // Fetch Members
-    let members = await (await fetch('/api/members')).json()
+    members = await (await fetch('/api/members')).json()
     
-    // Calculate & Set grid size
-    const root = Math.sqrt(members.length)
-    const wid = Math.ceil(root)
-    const hei = Math.round(root)
-    document.documentElement.style.setProperty('--width', wid)
-    document.documentElement.style.setProperty('--height', hei)
-
-
+    
+    redrawRows()
+    
+    
     // Setup Randomized Button Style Options
     const horizPos =
     {
@@ -54,7 +50,7 @@ async function run() {
         basics: [{ styleName: 'font-family', val: 'basics-serif' }],
     }
     const styleCatagories = [horizPos, verticalPos, font]
-
+    
     // Button toggling on and off styling
     const buttonStates = {
         false: [
@@ -66,20 +62,20 @@ async function run() {
             { styleName: 'box-shadow', val: 'inset 0 0 0 1000px rgba(255, 255, 255, 0.0), 0px 0px 15px 7px rgb(0, 255, 136)' },
         ]
     }
-
+    
     // Make member buttons 
     members.forEach(member => {
         // Init button
         let memberButton = document.createElement('person-button');
         memberButton.fullname = member.name;
         memberButton.id = member.fullname
-
+        
         // Set click toggle
         memberButton.onclick = async (click) => {
             // fullscreen()
-
+            
             buttonJustPressed = true;
-
+            
             let button = click.target;
             if (click.target.classList.contains('button-text')) {
                 button = click.target.parentElement;
@@ -98,12 +94,12 @@ async function run() {
                 await refreshMembers()
             }
         }
-
+        
         // Add name text
         let text = document.createElement('person-name')
         text.className = 'button-text'
         text.innerHTML = member.firstname
-
+        
         // Randomize mix and match text styles
         styleCatagories.forEach(styleCatagory => {
             let styleOptions = Object.values(styleCatagory)
@@ -113,22 +109,22 @@ async function run() {
                 text.style.setProperty(attribute.styleName, attribute.val)
             })
         })
-
+        
         // Do other adding and styling things
         memberButton.appendChild(text)
         memberButton.style.setProperty('background-image', `url(${member.img})`)
         if (!member.img) { memberButton.style.setProperty('background-image', `url(/assets/img/defaultpicture.jpg)`) }
         memberButton.className = 'button-in'
-
+        
         // Add button
         document.getElementById('button-grid').appendChild(memberButton)
     })
-
-
-
+    
+    
+    
     //////////////////////////////////////////////////////////////////////////
     // Test CONNECTIVITY
-
+    
     let noconnect = document.getElementById('noconnect');
     setInterval(async () => {
         if (await ping()) {
@@ -137,13 +133,13 @@ async function run() {
             noconnect.style.setProperty('visibility', 'visible')
         }
     }, 1000)
-
-
+    
+    
     //////////////////////////////////////////////////////////////////////////
-
-
+    
+    
     // Update logged in every 30 seconds
-
+    
     async function refreshMembers() {
         try {
             let membersIn = await cluckedIn()
@@ -154,18 +150,19 @@ async function run() {
                 button.loggedIn = button.fullname in membersIn
                 buttonStates[button.loggedIn].forEach(styleSpec => {
                     button.style.setProperty(styleSpec.styleName, styleSpec.val)
-
+                    
                 })
             }
         } catch (err) { console.log(err) }
+        redrawRows()
     }
-
-
-
+    
+    
+    
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
-
+    
     while (true) {
         // Get data and update buttons
         // Query data to map of { Name:TimeCluckedIn } 
@@ -176,9 +173,54 @@ async function run() {
             await sleep(2000);
         }
     }
-
+    
 }
 run()
+
+
+function redrawRows() {
+    // Compute number of rows and columns, and cell size
+    var n = members.length;
+    var x = document.body.scrollWidth
+    var y = document.body.scrollHeight
+    var ratio = x / y;
+    var ncols_float = Math.sqrt(n * ratio);
+    var nrows_float = n / ncols_float;
+    
+    // Find best option filling the whole height
+    var nrows1 = Math.ceil(nrows_float);
+    var ncols1 = Math.ceil(n / nrows1);
+    while (nrows1 * ratio < ncols1) {
+        nrows1++;
+        ncols1 = Math.ceil(n / nrows1);
+    }
+    var cell_size1 = y / nrows1;
+    
+    // Find best option filling the whole width
+    var ncols2 = Math.ceil(ncols_float);
+    var nrows2 = Math.ceil(n / ncols2);
+    while (ncols2 < nrows2 * ratio) {
+        ncols2++;
+        nrows2 = Math.ceil(n / ncols2);
+    }
+    var cell_size2 = x / ncols2;
+    
+    // Find the best values
+    var nrows, ncols, cell_size;
+    if (cell_size1 < cell_size2) {
+        nrows = nrows2;
+        ncols = ncols2;
+        cell_size = cell_size2;
+    } else {
+        nrows = nrows1;
+        ncols = ncols1;
+        cell_size = cell_size1;
+    }
+    
+    
+    document.documentElement.style.setProperty('--width', ncols)
+    document.documentElement.style.setProperty('--height', nrows)
+}
 
 setTimeout(async ()=>{
     await fetch('/api/members/refresh')
