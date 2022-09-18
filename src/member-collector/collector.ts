990d854f8c864f424df74d4b6c6349732ddb5b73
Zach R <zarutman+git@gmail.com>
2022-09-18 10:46:16 -0700

    make usernames case insensitive for profile pictures

--- src/member-collector/collector.ts
+++ src/member-collector/collector.ts
@@ -20,7 +20,7 @@ let collecting = false
 const client = new WebClient(slack_token);
 
 function tokenizeName(name:string):string {
-    return name.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/\s+/g,"")
+    return name.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/\s+/g,"").toLowerCase()
 }
 
 export const collect = async () => {
