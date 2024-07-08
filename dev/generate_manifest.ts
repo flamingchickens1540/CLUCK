8a902d6c0539234ae8b7918dc126270347e2df81
Zach R <zarutman+git@gmail.com>
2024-07-08 11:46:09 -0700

    fix: use config to generate dev manifests

--- dev/generate_manifest.ts
+++ dev/generate_manifest.ts
@@ -1,16 +1,14 @@
 import prod_manifest from './manifest.json'
 import fs from 'fs/promises'
-async function generate_dev(command_prefix: string) {
-    const dev_manifest = structuredClone(prod_manifest)
-    prod_manifest.features.slash_commands.forEach((command, i) => {
-        dev_manifest.features.slash_commands[i] = {
-            command: '/' + command_prefix + command.command.slice(1),
-            description: command.description,
-            should_escape: command.should_escape,
-            usage_hint: command.usage_hint
-        }
-    })
-    await fs.writeFile(`./dev/${command_prefix}manifest.json`, JSON.stringify(dev_manifest, null, 4))
-}
+import config from '@config'
 
-await generate_dev('dev_')
+const dev_manifest = structuredClone(prod_manifest)
+const prefix = config.slack.app.command_prefix
+if (prefix) {
+    dev_manifest.features.slash_commands.forEach((cmd) => {
+        cmd.command = '/' + prefix + '_' + cmd.command.slice(1)
+    })
+    dev_manifest.display_information.name += ` ${prefix}`
+    dev_manifest.features.bot_user.display_name += ` ${prefix}`
+}
+await fs.writeFile(`./dev/${prefix}.manifest.json`, JSON.stringify(dev_manifest, null, 4))
