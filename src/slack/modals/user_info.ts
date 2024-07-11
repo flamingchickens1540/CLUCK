4e087a806bd726f83dff6cc12c162022d3547f8a
Zach R <zarutman+git@gmail.com>
2024-07-10 17:01:59 -0700

    feat: improve user info modal

--- src/slack/modals/user_info.ts
+++ src/slack/modals/user_info.ts
@@ -1,6 +1,6 @@
 import { Member, Prisma } from '@prisma/client'
 import { Blocks, Elements, Modal } from 'slack-block-builder'
-import prisma from '~lib/prisma'
+import prisma, { getMemberPhoto } from '~lib/prisma'
 
 export async function getUserDataModal(input: Prisma.MemberWhereUniqueInput) {
     const member = await prisma.member.findUnique({ where: input })
@@ -9,26 +9,26 @@ export async function getUserDataModal(input: Prisma.MemberWhereUniqueInput) {
     }
 
     const createFieldSection = <T extends keyof Member>(label: string, field: T, converter: (v: Member[T]) => string) => {
-        return [Blocks.Section().fields(label, converter(member[field])), Blocks.Divider()]
+        return [Blocks.Section().fields('*' + label + '*', converter(member[field])), Blocks.Divider()]
     }
     // prettier-ignore
     return Modal()
         .title("Your Information")
         .blocks(
-            Blocks.Section().fields('*Field*', '*Value*'),
+            Blocks.Section()
+                .text(" ")
+                .accessory(Elements.Img().imageUrl(getMemberPhoto(member)).altText('Profile Picture')),
+            Blocks.Divider(),
             ...createFieldSection("Join Date", 'createdAt', v => v.toLocaleDateString()),
             ...createFieldSection("Email", 'email', v => v!),
             ...createFieldSection("Slack ID", 'slack_id', v => v!),
+            ...createFieldSection("First Name", 'first_name', v => v!),
             ...createFieldSection("Full Name", 'full_name', v => v!),
             ...createFieldSection("Grade", 'grade', v => v.toString()),
             ...createFieldSection("Experience", 'years', v => v+" years"),
             ...createFieldSection("Slack Photo Approved", 'use_slack_photo', v => v ? "Yes" : "No"),
-            Blocks.Section()
-                .text("Slack Photo")
-                .accessory(Elements.Img().imageUrl(member.slack_photo ?? "").altText('Profile Picture')),
-            Blocks.Section()
-                .text("School Photo")
-                .accessory(Elements.Img().imageUrl(member.fallback_photo ?? "").altText('School Photo'))
+            Blocks.Context().elements('Last modified ' + member.updatedAt.toLocaleString())
+
 
         )
 }
