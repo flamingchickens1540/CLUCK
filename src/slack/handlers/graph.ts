ce39c09d84565e2ee7260a319d32bad0467cab35
Zach R <zarutman+git@gmail.com>
2024-07-11 10:58:16 -0700

    feat: improve graph command responses

--- src/slack/handlers/graph.ts
+++ src/slack/handlers/graph.ts
@@ -1,10 +1,10 @@
 import type { AllMiddlewareArgs, SlackCommandMiddlewareArgs } from '@slack/bolt'
 import { Blocks, Message } from 'slack-block-builder'
 import { createHourChartForTeam, createHourChartForUsers } from '~slack/lib/chart'
+import { formatList } from '~slack/lib/messages'
 
 export async function handleGraphCommand({ command, ack, respond, client }: SlackCommandMiddlewareArgs & AllMiddlewareArgs) {
-    await ack({ response_type: 'ephemeral', text: 'Generating graph...' })
-
+    await ack()
     const text = command.text.trim()
     const teamLabels = {
         all: 'all members',
@@ -12,38 +12,34 @@ export async function handleGraphCommand({ command, ack, respond, client }: Slac
         junior: 'Junior Team'
     }
     const team = text as keyof typeof teamLabels
+
+    const resp = { text: '', url: '', title: 'Hours Graph' }
+    await respond({ response_type: 'ephemeral', text: 'Generating graph...' })
     if (teamLabels[team]) {
-        const { url } = await createHourChartForTeam(text as keyof typeof teamLabels)
-        const msg = Message()
-            .blocks(
-                Blocks.Section().text(':chart_with_upwards_trend: <@' + command.user_id + '> generated a graph for ' + teamLabels[team]),
-                Blocks.Image().title('Hours Graph').imageUrl(url).altText('Hours Graph')
-            )
-            .buildToObject()
-        await respond(msg)
-        return
-    }
-    const users: Set<string> = new Set()
-    const user_matches = text.matchAll(/<@(\w+)\|\w.+?>/g)
-    for (const user of user_matches) {
-        users.add(user[1])
-    }
+        const { url, success } = await createHourChartForTeam(text as keyof typeof teamLabels)
+        resp.text = ':chart_with_upwards_trend: <@' + command.user_id + '> generated a graph for ' + teamLabels[team]
+        resp.title = success ? 'Hours Graph' : 'You were hourless to stop me from making this pun'
+        resp.url = url
+    } else {
+        const users: Set<string> = new Set()
+        const user_matches = text.matchAll(/<@(\w+)\|\w.+?>/g)
+        for (const user of user_matches) {
+            users.add(user[1])
+        }
 
-    if (users.size == 0) {
-        await respond({ replace_original: true, response_type: 'ephemeral', text: 'No users specified' })
-        return
+        if (users.size == 0) {
+            users.add(command.user_id)
+        }
+
+        const { url, success } = await createHourChartForUsers([...users])
+        resp.url = url
+        resp.title = success ? 'Hours Graph' : 'You were hourless to stop me from making this pun'
+        resp.text = ':chart_with_upwards_trend: <@' + command.user_id + '> generated a graph for ' + formatList([...users].map((u) => `<@${u}>`))
     }
-
-    const { url, success } = await createHourChartForUsers([...users])
-
     const msg = Message()
-        .blocks(
-            Blocks.Section().text(':chart_with_upwards_trend: <@' + command.user_id + '> generated a graph for ' + [...users].map((u) => '<@' + u + '>').join(', ')),
-            Blocks.Image()
-                .title(success ? 'Hours Graph' : 'You were hourless to stop me from making this pun')
-                .imageUrl(url)
-                .altText('Hours Graph')
-        )
+        .text(resp.text)
+        .blocks(Blocks.Section().text(resp.text), Blocks.Image().title(resp.title).imageUrl(resp.url).altText('Hours Graph'))
+        .inChannel()
         .buildToObject()
     await respond(msg)
 }
