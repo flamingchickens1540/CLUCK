275b74154164462827cd586d390feba93fcf0e34
Zach R <zarutman+git@gmail.com>
2024-09-01 14:50:19 -0700

    fix: don't reactivate automatically

--- src/tasks/slack.ts
+++ src/tasks/slack.ts
@@ -37,7 +37,7 @@ export async function syncSlackMembers() {
                             slack_photo: slack_member.profile?.image_original,
                             slack_photo_small: slack_member.profile?.image_192,
                             first_name: display_name.split(' ')[0].trim(),
-                            active: students_set.has(slack_member.id!) && !slack_member?.deleted
+                            active: member.active && students_set.has(slack_member.id!) && !slack_member?.deleted
                         }
                     })
                     updated++
