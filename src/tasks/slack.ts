c04c273dceea72549f1be681c7a79112117b0e0e
Zach R <zarutman+git@gmail.com>
2024-09-01 14:56:51 -0700

    fix: don't move around existing slack members

--- src/tasks/slack.ts
+++ src/tasks/slack.ts
@@ -18,16 +18,18 @@ export async function syncSlackMembers() {
             const db_members = await prisma.member.findMany()
             const slack_members = (await slack_client.users.list({})).members ?? []
             const slack_members_lookup: Record<string, SlackMember> = {}
+            const slack_members_lookup_id: Record<string, SlackMember> = {}
             slack_members.forEach((member) => {
                 if (member.profile?.email) {
                     slack_members_lookup[member.profile.email.toLowerCase().trim()] = member
+                    slack_members_lookup_id[member.id!] = member
                 }
             })
             let updated = 0
             const students_group = await slack_client.usergroups.users.list({ usergroup: config.slack.groups.students })
             const students_set = new Set<string>(students_group.users ?? [])
             for (const member of db_members) {
-                const slack_member = slack_members_lookup[member.email]
+                const slack_member = (member.slack_id != null ? slack_members_lookup_id[member.slack_id] : null) ?? slack_members_lookup[member.email]
                 if (slack_member != null) {
                     const display_name = ((slack_member.profile?.display_name?.length ?? 0) > 0 ? slack_member.profile?.display_name : slack_member.real_name) ?? member.full_name
                     await prisma.member.update({
