a048bef47826ee6603a841c025cd0d629fff9e2d
Zach R <zarutman+git@gmail.com>
2025-09-27 20:57:06 -0700

    fix: actually don't error

--- src/tasks/slack.ts
+++ src/tasks/slack.ts
@@ -37,57 +37,56 @@ export async function syncSlackMembers() {
     if (lock.isBusy()) {
         return
     }
-    await lock
-        .acquire('slack_sync', async () => {
-            logger.info('Starting slack sync...')
-            const db_members = await prisma.member.findMany()
-            const slack_members = (await slack_client.users.list({})).members ?? []
-            const slack_members_lookup_email: Record<string, SlackMember> = {}
-            const slack_members_lookup_id: Record<string, SlackMember> = {}
-            slack_members.forEach((member) => {
-                if (member.profile?.email) {
-                    slack_members_lookup_email[member.profile.email.toLowerCase().trim()] = member
-                    slack_members_lookup_id[member.id!] = member
-                }
-            })
-            let updated = 0
-            const active = new Map<string, Team>()
-            for (const [team, usergroup] of teams) {
-                const members = await slack_client.usergroups.users.list({ usergroup: usergroup }).catch(() => null)
-                if (members?.users == null || !members.ok) {
-                    logger.error({ members }, 'Could not fetch group members for ' + team)
-                    continue
-                }
-                for (const member of members.users) {
-                    if (active.has(member)) {
-                        if (team != 'unaffiliated') {
-                            logger.warn({ member, new: team, existing: active.get(member) }, 'Member affiliated with two teams')
-                        }
-                    } else {
-                        active.set(member, team as Team)
-                    }
-                }
+    await lock.acquire('slack_sync', async () => {
+        logger.info('Starting slack sync...')
+        const db_members = await prisma.member.findMany()
+        const slack_members = (await slack_client.users.list({})).members ?? []
+        const slack_members_lookup_email: Record<string, SlackMember> = {}
+        const slack_members_lookup_id: Record<string, SlackMember> = {}
+        slack_members.forEach((member) => {
+            if (member.profile?.email) {
+                slack_members_lookup_email[member.profile.email.toLowerCase().trim()] = member
+                slack_members_lookup_id[member.id!] = member
             }
-            for (const member of db_members) {
-                const slack_member = (member.slack_id != null ? slack_members_lookup_id[member.slack_id] : null) ?? slack_members_lookup_email[member.email]
-                if (slack_member != null) {
-                    const display_name = ((slack_member.profile?.display_name?.length ?? 0) > 0 ? slack_member.profile?.display_name : slack_member.real_name) ?? member.full_name
-                    const team = active.get(slack_member.id!)
-                    await prisma.member.update({
-                        where: { email: member.email },
-                        data: {
-                            slack_id: slack_member.id,
-                            slack_photo: slack_member.profile?.image_original,
-                            slack_photo_small: slack_member.profile?.image_192,
-                            first_name: display_name.split(' ')[0].trim(),
-                            full_name: slack_member.profile!.real_name!,
-                            team: team,
-                            active: team != null && !slack_member?.deleted
-                        }
-                    })
-                    updated++
-                }
-            }
-            logger.info(`Found ${updated} members on slack`)
         })
+        let updated = 0
+        const active = new Map<string, Team>()
+        for (const [team, usergroup] of teams) {
+            const members = await slack_client.usergroups.users.list({ usergroup: usergroup }).catch(() => null)
+            if (members?.users == null || !members.ok) {
+                logger.error({ members }, 'Could not fetch group members for ' + team)
+                continue
+            }
+            for (const member of members.users) {
+                if (active.has(member)) {
+                    if (team != 'unaffiliated') {
+                        logger.warn({ member, new: team, existing: active.get(member) }, 'Member affiliated with two teams')
+                    }
+                } else {
+                    active.set(member, team as Team)
+                }
+            }
+        }
+        for (const member of db_members) {
+            const slack_member = (member.slack_id != null ? slack_members_lookup_id[member.slack_id] : null) ?? slack_members_lookup_email[member.email]
+            if (slack_member != null) {
+                const display_name = ((slack_member.profile?.display_name?.length ?? 0) > 0 ? slack_member.profile?.display_name : slack_member.real_name) ?? member.full_name
+                const team = active.get(slack_member.id!)
+                await prisma.member.update({
+                    where: { email: member.email },
+                    data: {
+                        slack_id: slack_member.id,
+                        slack_photo: slack_member.profile?.image_original,
+                        slack_photo_small: slack_member.profile?.image_192,
+                        first_name: display_name.split(' ')[0].trim(),
+                        full_name: slack_member.profile!.real_name!,
+                        team: team,
+                        active: team != null && !slack_member?.deleted
+                    }
+                })
+                updated++
+            }
+        }
+        logger.info(`Found ${updated} members on slack`)
+    })
 }
